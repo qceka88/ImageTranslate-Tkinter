349149caa47a339fffc8da050dd9b6d610a921d2
Yanko Rashkov <thewhitedead@abv.bg>
2023-09-04 23:15:44 +0300

    update

--- user_interface.py
+++ user_interface.py
@@ -1,31 +1,55 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
 
+from text_extraction import TextExtractor
+
+
+# TODO: Implement spaghetti code logic in nice UI/UX
+
+# TODO: Add boxes for source language and target language
+# TODO: fix file save function, to save extracted text
+# TODO: add option for user to "Straigth" tilt images
+# TODO: if user not select "Straight" option, program should directly start extracting of text
 
 class ImageTextTranslator:
 
     def __init__(self):
         self.interface = tk.Tk()
-        self.interface_window_style()
+        self.interface_window()
         self.interface.mainloop()
 
-    def interface_window_style(self):
+    def interface_window(self):
         self.interface.geometry('500x300')
         self.interface.title('Image Text Translation')
         self.interface.maxsize(1920, 1080)
         self.interface.minsize(1024, 768)
         self.interface['bg'] = 'white'
-        self.button = ttk.Button(self.interface, text="Upload Image", command=self.file_open)
-        self.button.grid(column=1, row=1)
-        self.button = ttk.Button(self.interface, text="Save Text", command=self.file_save)
-        self.button.grid(column=100, row=100)
+        self.original_image = None
+        self.button = ttk.Button(self.interface, text="Upload Image", command=self.Upload_image)
+        self.button.grid(column=150, row=1)
 
-    def file_open(self):
-        self.file = filedialog.askopenfilename()
+        self.button = ttk.Button(self.interface, text="Extract Text", command=self.Extract_text)
+        self.button.grid(column=150, row=250)
 
-    def file_save(self):
+    def Upload_image(self):
+        self.original_image = filedialog.askopenfilename()
+
+    def File_save(self):
         self.file = filedialog.asksaveasfile()
 
+    def Extract_text(self):
+        extracted_text = TextExtractor(self.original_image)
+
+        def Result_window():
+            self.text_window = tk.Toplevel()
+            self.text_window.geometry("1024x768")
+            self.text_window.title('Extracted Text')
+            self.button = ttk.Button(self.text_window, text="Save Text", command=self.File_save)
+            self.button.grid(column=1, row=1)
+
+            self.text_window.mainloop()
+
+        Result_window()
 
 
 if __name__ == '__main__':
